7437adb3cbfbbc7d164205fea2e8e6740705cc0b
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-03-20 18:14:02 -0400

    Added some more Telescope keybindings.

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -16,6 +16,10 @@ vim.keymap.set({ "n", "v" }, "<leader>p", "\"*p")
 -- Telescope keymaps.
 local telescope_builtin = require("telescope.builtin")
 vim.keymap.set("n", "<C-p>", telescope_builtin.find_files)
+vim.keymap.set("n", "<C-g>", telescope_builtin.git_files)
+vim.keymap.set("n", "<leader>ts", function()
+    telescope_builtin.treesitter({ show_line = true })
+end)
 
 -- Harpoon keymaps
 local harpoon_ui = require("harpoon.ui")
