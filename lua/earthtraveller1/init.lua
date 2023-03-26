3e7b33c451268b9e934fe4cf888db6a298741a08
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-03-26 15:38:09 -0400

    Moved init.lua into earthraveller1 directory.

---
+++ lua/earthtraveller1/init.lua
@@ -0,0 +1,225 @@
+require("earthtraveller1.plugins")
+require("earthtraveller1.keymaps")
+
+-- Place where all my settings go
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.autoread = true
+
+vim.opt.scrolloff = 8
+
+-- Disabled netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Treesitter settings
+require'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "c", "lua", "rust", "cpp", "typescript", "python", "help", "query" },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+
+    -- List of parsers to ignore installing (for "all")
+    --     ignore_install = { "javascript" },
+
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+    highlight = {
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
+}
+
+-- The setup config table shows all available config options with their default values:
+require("presence").setup({
+    -- General options
+    auto_update         = true,                       -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+    neovim_image_text   = "The One True Text Editor", -- Text displayed when hovered over the Neovim image
+    main_image          = "neovim",                   -- Main image display (either "neovim" or "file")
+    -- client_id           = "793271441293967371",       -- Use your own Discord application client id (not recommended)
+    log_level           = nil,                        -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+    debounce_timeout    = 10,                         -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+    enable_line_number  = false,                      -- Displays the current line number instead of the current project
+    blacklist           = {},                         -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+    buttons             = true,                       -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+    file_assets         = {},                         -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+    show_time           = true,                       -- Show the timer
+
+    -- Rich Presence text options
+    editing_text        = "Editing %s",               -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+    file_explorer_text  = "Browsing %s",              -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+    git_commit_text     = "Committing changes",       -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+    plugin_manager_text = "Managing plugins",         -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+    reading_text        = "Reading %s",               -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+    workspace_text      = "Working on %s",            -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+    line_number_text    = "Line %s out of %s",        -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+})
+
+-- Configuring the LSP.
+local lsp = require('lsp-zero').preset({
+  name = 'recommended',
+  set_lsp_keymaps = true,
+  manage_nvim_cmp = true,
+  suggest_lsp_servers = false,
+})
+
+-- (Optional) Configure lua language server for neovim
+lsp.nvim_workspace()
+
+lsp.setup()
+
+require("nvim-tree").setup()
+
+-- Set barbar's options
+require'bufferline'.setup {
+    -- Enable/disable animations
+    animation = false,
+
+    -- Enable/disable auto-hiding the tab bar when there is a single buffer
+    auto_hide = false,
+
+    -- Enable/disable current/total tabpages indicator (top right corner)
+    tabpages = true,
+
+    -- Enable/disable close button
+    closable = true,
+
+    -- Enables/disable clickable tabs
+    --  - left-click: go to buffer
+    --  - middle-click: delete buffer
+    clickable = true,
+
+    -- Enables / disables diagnostic symbols
+    diagnostics = {
+        -- you can use a list
+        {enabled = true, icon = 'ﬀ'}, -- ERROR
+        {enabled = false}, -- WARN
+        {enabled = false}, -- INFO
+        {enabled = true},  -- HINT
+
+        -- OR `vim.diagnostic.severity`
+        [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
+        [vim.diagnostic.severity.WARN] = {enabled = false},
+        [vim.diagnostic.severity.INFO] = {enabled = false},
+        [vim.diagnostic.severity.HINT] = {enabled = true},
+    },
+
+    -- Excludes buffers from the tabline
+    exclude_ft = {'javascript'},
+    exclude_name = {'package.json'},
+
+    -- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
+    hide = {extensions = false, inactive = false},
+
+    -- Disable highlighting alternate buffers
+    highlight_alternate = false,
+
+    -- Disable highlighting file icons in inactive buffers
+    highlight_inactive_file_icons = false,
+
+    -- Enable highlighting visible buffers
+    highlight_visible = true,
+
+    -- Enable/disable icons
+    -- if set to 'numbers', will show buffer index in the tabline
+    -- if set to 'both', will show buffer index and icons in the tabline
+    icons = true,
+
+    -- If set, the icon color will follow its corresponding buffer
+    -- highlight group. By default, the Buffer*Icon group is linked to the
+    -- Buffer* group (see Highlighting below). Otherwise, it will take its
+    -- default value as defined by devicons.
+    icon_custom_colors = false,
+
+    -- Configure icons on the bufferline.
+    icon_separator_active = '▎',
+    icon_separator_inactive = '▎',
+    icon_close_tab = '',
+    icon_close_tab_modified = '●',
+    icon_pinned = '車',
+
+    -- If true, new buffers will be inserted at the start/end of the list.
+    -- Default is to insert after current buffer.
+    insert_at_end = true,
+    insert_at_start = false,
+
+    -- Sets the maximum padding width with which to surround each tab
+    maximum_padding = 1,
+
+    -- Sets the minimum padding width with which to surround each tab
+    minimum_padding = 1,
+
+    -- Sets the maximum buffer name length.
+    maximum_length = 30,
+
+    -- If set, the letters for each buffer in buffer-pick mode will be
+    -- assigned based on their name. Otherwise or in case all letters are
+    -- already assigned, the behavior is to assign letters in order of
+    -- usability (see order below)
+    semantic_letters = true,
+
+    -- New buffer letters are assigned in this order. This order is
+    -- optimal for the qwerty keyboard layout but might need adjustement
+    -- for other layouts.
+    letters = 'asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP',
+
+    -- Sets the name of unnamed buffers. By default format is "[Buffer X]"
+    -- where X is the buffer number. But only a static string is accepted here.
+    no_name_title = nil,
+}
+
+-- Make it so that the tabs are offset
+vim.api.nvim_create_autocmd('FileType', {
+    callback = function(tbl)
+        local set_offset = require('bufferline.api').set_offset
+
+        local bufwinid
+        local last_width
+        local autocmd = vim.api.nvim_create_autocmd('WinScrolled', {
+            callback = function()
+                bufwinid = bufwinid or vim.fn.bufwinid(tbl.buf)
+
+                local width = vim.api.nvim_win_get_width(bufwinid)
+                if width ~= last_width then
+                    set_offset(width, 'FileTree')
+                    last_width = width
+                end
+            end,
+        })
+
+        vim.api.nvim_create_autocmd('BufWipeout', {
+            buffer = tbl.buf,
+            callback = function()
+                vim.api.nvim_del_autocmd(autocmd)
+                set_offset(0)
+            end,
+            once = true,
+        })
+    end,
+    pattern = 'NvimTree', -- or any other filetree's `ft`
+})
+
+require("catppuccin").setup({
+    flavour = "mocha",
+    transparent_background = true
+})
+
+vim.cmd.colorscheme("catppuccin")
+
+-- Opens the file tree on startup, because why not?
+vim.cmd("NvimTreeToggle")
