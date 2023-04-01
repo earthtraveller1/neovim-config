e4dd8713e3964633307bdc7611b1aeae7b667a5a
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-01 17:04:18 -0400

    Updated barbar configuration.

--- after/plugin/barbar.lua
+++ after/plugin/barbar.lua
@@ -42,7 +42,7 @@ map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 -- :BarbarEnable - enables barbar (enabled by default)
 -- :BarbarDisable - very bad command, should never be used
 
-require'bufferline'.setup {
+require 'bufferline'.setup {
     -- Enable/disable animations
     animation = false,
 
@@ -60,27 +60,12 @@ require'bufferline'.setup {
     --  - middle-click: delete buffer
     clickable = true,
 
-    -- Enables / disables diagnostic symbols
-    diagnostics = {
-        -- you can use a list
-        {enabled = true, icon = 'ﬀ'}, -- ERROR
-        {enabled = false}, -- WARN
-        {enabled = false}, -- INFO
-        {enabled = true},  -- HINT
-
-        -- OR `vim.diagnostic.severity`
-        [vim.diagnostic.severity.ERROR] = {enabled = true, icon = 'ﬀ'},
-        [vim.diagnostic.severity.WARN] = {enabled = false},
-        [vim.diagnostic.severity.INFO] = {enabled = false},
-        [vim.diagnostic.severity.HINT] = {enabled = true},
-    },
-
     -- Excludes buffers from the tabline
-    exclude_ft = {'javascript'},
-    exclude_name = {'package.json'},
+    exclude_ft = { 'javascript' },
+    exclude_name = { 'package.json' },
 
     -- Hide inactive buffers and file extensions. Other options are `alternate`, `current`, and `visible`.
-    hide = {extensions = false, inactive = false},
+    hide = { extensions = false, inactive = false },
 
     -- Disable highlighting alternate buffers
     highlight_alternate = false,
@@ -91,23 +76,37 @@ require'bufferline'.setup {
     -- Enable highlighting visible buffers
     highlight_visible = true,
 
-    -- Enable/disable icons
-    -- if set to 'numbers', will show buffer index in the tabline
-    -- if set to 'both', will show buffer index and icons in the tabline
-    icons = true,
-
-    -- If set, the icon color will follow its corresponding buffer
-    -- highlight group. By default, the Buffer*Icon group is linked to the
-    -- Buffer* group (see Highlighting below). Otherwise, it will take its
-    -- default value as defined by devicons.
-    icon_custom_colors = false,
-
-    -- Configure icons on the bufferline.
-    icon_separator_active = '▎',
-    icon_separator_inactive = '▎',
-    icon_close_tab = '',
-    icon_close_tab_modified = '●',
-    icon_pinned = '車',
+    icons = {
+        -- Configure the base icons on the bufferline.
+        buffer_index = false,
+        buffer_number = false,
+        button = '',
+        -- Enables / disables diagnostic symbols
+        diagnostics = {
+            [vim.diagnostic.severity.ERROR] = { enabled = true, icon = 'ﬀ' },
+            [vim.diagnostic.severity.WARN] = { enabled = false },
+            [vim.diagnostic.severity.INFO] = { enabled = false },
+            [vim.diagnostic.severity.HINT] = { enabled = true },
+        },
+        filetype = {
+            -- Sets the icon's highlight group.
+            -- If false, will use nvim-web-devicons colors
+            custom_colors = false,
+            -- Requires `nvim-web-devicons` if `true`
+            enabled = true,
+        },
+        separator = { left = '▎', right = '' },
+        -- Configure the icons on the bufferline when modified or pinned.
+        -- Supports all the base icon options.
+        modified = { button = '●' },
+        pinned = { button = '車' },
+        -- Configure the icons on the bufferline based on the visibility of a buffer.
+        -- Supports all the base icon options, plus `modified` and `pinned`.
+        alternate = { filetype = { enabled = false } },
+        current = { buffer_index = true },
+        inactive = { button = '×' },
+        visible = { modified = { buffer_number = false } },
+    },
 
     -- If true, new buffers will be inserted at the start/end of the list.
     -- Default is to insert after current buffer.
