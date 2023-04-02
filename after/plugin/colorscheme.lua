cdca692c5c050529c7b43140e691caa5ab9f587d
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-04-02 11:32:14 -0400

    Disabled transparent background on Windows.

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -1,3 +1,9 @@
+local transparent_background = true
+
+if jit.os == "Windows" then
+    transparent_background = false
+end
+
 require("catppuccin").setup({
     flavour = "mocha",
     transparent_background = true
