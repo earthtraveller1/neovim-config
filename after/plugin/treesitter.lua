b1d65ee06623127c1a3b974fabc96cc9dc476b8e
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-04-02 11:30:04 -0400

    Removed vimdoc as ensure installed.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -4,7 +4,7 @@ vim.g.loaded_netrwPlugin = 1
 
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "c", "lua", "vim", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
