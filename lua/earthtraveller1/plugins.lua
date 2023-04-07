b923f86d3c70002c6077959357927883192ba328
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-04-07 09:12:22 -0400

    Changed barbar to bufferline.

--- lua/earthtraveller1/plugins.lua
+++ lua/earthtraveller1/plugins.lua
@@ -62,7 +62,7 @@ local function plugins(use)
         use { "ThePrimeagen/harpoon", requires = { { 'nvim-lua/plenary.nvim' }} }
 
         -- Tabs.
-        use {'romgrk/barbar.nvim', requires = 'nvim-web-devicons'}
+        use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
 
         -- Git integration.
         use "tpope/vim-fugitive"
