e6f31dec7cf933db328e44f1730ac1de55ffbc19
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-10 21:26:30 -0400

    Installed the lsp_signature plugin.

--- lua/earthtraveller1/plugins.lua
+++ lua/earthtraveller1/plugins.lua
@@ -47,6 +47,7 @@ local function plugins(use)
                 {'hrsh7th/cmp-path'},         -- Optional
                 {'saadparwaiz1/cmp_luasnip'}, -- Optional
                 {'hrsh7th/cmp-nvim-lua'},     -- Optional
+                {'ray-x/lsp_signature.nvim'}, -- Optional
 
                 -- Snippets
                 {'L3MON4D3/LuaSnip'},             -- Required
