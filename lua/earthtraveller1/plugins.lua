2749d912f24ef2133edfa4331f82621cdc7d4ebb
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-16 15:07:11 -0400

    Moved some of the configuration into the plugins.lua file.

--- lua/earthtraveller1/plugins.lua
+++ lua/earthtraveller1/plugins.lua
@@ -18,7 +18,31 @@ require("lazy").setup({
         "nvim-treesitter/nvim-treesitter",
         build = function()
             require("nvim-treesitter.install").update({ with_sync = true })
-        end
+        end,
+        opts = {
+            -- A list of parser names, or "all" (the five listed parsers should always be installed)
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "cpp", "python", "javascript",
+                "typescript" },
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+            -- List of parsers to ignore installing (for "all")
+            -- ignore_install = { "javascript" },
+
+            ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+            -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+            highlight = {
+                enable = true,
+                -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+                -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+                -- Using this option may slow down your editor, and you may see some duplicate highlights.
+                -- Instead of true it can also be a list of languages
+                additional_vim_regex_highlighting = false,
+            },
+        }
     },
     { "catppuccin/nvim", name = "catppuccin" },
     {
@@ -26,25 +50,65 @@ require("lazy").setup({
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' },    -- Required
+            { 'neovim/nvim-lspconfig' },             -- Required
             { 'williamboman/mason.nvim',          build = vim.cmd.MasonUpdate },
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/nvim-cmp' },     -- Required
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
         }
     },
     {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.1',
-        dependencies = { 'nvim-lua/plenary.nvim' }
+        dependencies = { 'nvim-lua/plenary.nvim' },
     },
     'tpope/vim-fugitive',
     {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        opts = {
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                globalstatus = false,
+                refresh = {
+                    statusline = 1000,
+                    tabline = 1000,
+                    winbar = 1000,
+                }
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype' },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+            inactive_sections = {
+                lualine_a = {},
+                lualine_b = {},
+                lualine_c = { 'filename' },
+                lualine_x = { 'location' },
+                lualine_y = {},
+                lualine_z = {}
+            },
+            tabline = {},
+            winbar = {},
+            inactive_winbar = {},
+            extensions = {}
+        }
     },
     {
         'romgrk/barbar.nvim',
@@ -148,9 +212,7 @@ require("lazy").setup({
     'nvim-tree/nvim-tree.lua',
 })
 
-require("earthtraveller1.plugins.treesitter")
 require("earthtraveller1.plugins.lsp")
 require("earthtraveller1.plugins.telescope")
-require("earthtraveller1.plugins.lualine")
 require("earthtraveller1.plugins.barbar")
 require("earthtraveller1.plugins.nvim-tree")
