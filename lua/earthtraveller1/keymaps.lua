2341d74fc0dd4ac4c92f75a240bc6bc2b05e7ccd
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-04-16 18:49:06 -0400

    Made the Git-fugitive window bigger.

--- lua/earthtraveller1/keymaps.lua
+++ lua/earthtraveller1/keymaps.lua
@@ -11,7 +11,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("n", "<leader>qq", "<Cmd>wqa<CR>")
 
 -- Opening fugitive in a better way.
-vim.keymap.set("n", "<leader>gi", "<Cmd>Git<CR><C-w>L<C-w>60<")
+vim.keymap.set("n", "<leader>gi", "<Cmd>Git<CR><C-w>L")
 
 -- Opening the symbol outline.
 vim.keymap.set("n", "<leader>so", "<Cmd>SymbolsOutline<CR>")
