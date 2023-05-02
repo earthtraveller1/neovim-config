745e67c7a5712fbd0689a09039a2fb568bfdc18d
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-05-02 15:50:12 -0400

    Moved catppuccin configuration to it's own separate file.

--- lua/earthtraveller1/plugins.lua
+++ lua/earthtraveller1/plugins.lua
@@ -20,53 +20,7 @@ require("lazy").setup({
             require("nvim-treesitter.install").update({ with_sync = true })
         end,
     },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        config = {
-            flavour = "mocha", -- latte, frappe, macchiato, mocha
-            background = {
-                -- :h background
-                light = "latte",
-                dark = "mocha",
-            },
-            transparent_background = true,
-            show_end_of_buffer = false, -- show the '~' characters after the end of buffers
-            term_colors = false,
-            dim_inactive = {
-                enabled = false,
-                shade = "dark",
-                percentage = 0.15,
-            },
-            no_italic = false, -- Force no italic
-            no_bold = false,   -- Force no bold
-            styles = {
-                comments = { "italic" },
-                conditionals = { "italic" },
-                loops = {},
-                functions = {},
-                keywords = {},
-                strings = {},
-                variables = {},
-                numbers = {},
-                booleans = {},
-                properties = {},
-                types = {},
-                operators = {},
-            },
-            color_overrides = {},
-            custom_highlights = {},
-            integrations = {
-                cmp = true,
-                gitsigns = true,
-                nvimtree = true,
-                telescope = true,
-                notify = false,
-                mini = false,
-                -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-            },
-        }
-    },
+    { "catppuccin/nvim", name = "catppuccin" },
     {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
@@ -102,6 +56,7 @@ require("lazy").setup({
 })
 
 require("earthtraveller1.plugins.barbar")
+require("earthtraveller1.plugins.catppuccin")
 require("earthtraveller1.plugins.debugger")
 require("earthtraveller1.plugins.lsp")
 require("earthtraveller1.plugins.lualine")
