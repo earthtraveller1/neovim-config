77b4c7c99b3d3d2fd3b11b72ced4e5021f83d040
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-05-06 08:46:52 -0400

    Enabled autohiding the bufferline when only one buffer is open.

--- lua/earthtraveller1/plugins/barbar.lua
+++ lua/earthtraveller1/plugins/barbar.lua
@@ -8,7 +8,7 @@ require 'barbar'.setup {
     animation = true,
 
     -- Enable/disable auto-hiding the tab bar when there is a single buffer
-    auto_hide = false,
+    auto_hide = true,
 
     -- Enable/disable current/total tabpages indicator (top right corner)
     tabpages = true,
