cb0c07c6b8668f04974041192397815bd3b167ef
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-05-09 16:30:35 -0400

    Moved the settings require statement above everything else.

--- lua/earthtraveller1/init.lua
+++ lua/earthtraveller1/init.lua
@@ -1,9 +1,4 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
-vim.opt.termguicolors = true
-
+require("earthtraveller1.settings")
 require("earthtraveller1.keymaps")
 require("earthtraveller1.plugins")
-require("earthtraveller1.settings")
 
