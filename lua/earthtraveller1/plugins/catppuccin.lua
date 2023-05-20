ff8a146aec5be242778de8f60700a5acd865fe59
Liangyue " Yue <liangyuezhao7@gmail.com>
2023-05-20 12:38:16 -0400

    Disabled transparent backgrounds.

--- lua/earthtraveller1/plugins/catppuccin.lua
+++ lua/earthtraveller1/plugins/catppuccin.lua
@@ -1,20 +1,20 @@
 require("catppuccin").setup {
-    flavour = "mocha",         -- latte, frappe, macchiato, mocha
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
     background = {
         -- :h background
         light = "latte",
         dark = "mocha",
     },
-    transparent_background = true,
-    show_end_of_buffer = false,         -- show the '~' characters after the end of buffers
+    transparent_background = false,
+    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
     term_colors = false,
     dim_inactive = {
-        enabled = false,
+        enabled = true,
         shade = "dark",
         percentage = 0.15,
     },
-    no_italic = false,         -- Force no italic
-    no_bold = false,           -- Force no bold
+    no_italic = false, -- Force no italic
+    no_bold = false,   -- Force no bold
     styles = {
         comments = { "italic" },
         conditionals = { "italic" },
