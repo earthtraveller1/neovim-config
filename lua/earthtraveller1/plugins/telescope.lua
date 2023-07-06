c5e099825b0b8536fdded7690f3ecd22b6ef8fff
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-06 14:56:22 -0400

    Enabled finding hidden files.

--- lua/earthtraveller1/plugins/telescope.lua
+++ lua/earthtraveller1/plugins/telescope.lua
@@ -30,8 +30,8 @@ require('telescope').setup {
 }
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ hidden = true }) end, {})
+vim.keymap.set('n', '<leader>ft', function() builtin.treesitter({ hidden = true }) end, {})
+vim.keymap.set('n', '<leader>fg', function() builtin.live_grep({ hidden = true }) end, {})
+vim.keymap.set('n', '<leader>fb', function() builtin.buffers({ hidden = true }) end, {})
+vim.keymap.set('n', '<leader>fh', function() builtin.help_tags({ hidden = true }) end, {})
