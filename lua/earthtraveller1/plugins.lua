873dbc42616f79b7dc93638f26e65f2fea43825f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-11 17:18:51 -0400

    Installed the nvim-surround plugin.

--- lua/earthtraveller1/plugins.lua
+++ lua/earthtraveller1/plugins.lua
@@ -53,6 +53,17 @@ require("lazy").setup({
             'nvim-lua/plenary.nvim' }
     },
 
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
     { "catppuccin/nvim",           name = "catppuccin" },
     { "j-hui/fidget.nvim",         tag = "legacy",                                  opts = {} },
     { 'nvim-lualine/lualine.nvim', dependencies = { 'nvim-tree/nvim-web-devicons' } },
