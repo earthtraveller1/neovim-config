a9f37acd776a8b0b33d1bab8320066eec2745e9a
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-14 14:39:41 -0400

    Fixed everything.

--- lua/earthtraveller1/plugins/git-worktrees.lua
+++ lua/earthtraveller1/plugins/git-worktrees.lua
@@ -1,4 +1,4 @@
 local telescope = require('telescope')
 
-vim.keymap.set("n", "<leader>fw", telescope.extensions.git_worktrees.git_worktrees)
-vim.keymap.set("n", "<leader>cw", telescope.extensions.git_worktrees.git_worktrees)
+vim.keymap.set("n", "<leader>fw", telescope.extensions.git_worktree.git_worktrees)
+vim.keymap.set("n", "<leader>cw", telescope.extensions.git_worktree.create_git_worktree)
