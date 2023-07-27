1646c0490dbf7c6b8715f4ce97e9c3b75f43f1ec
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-27 15:12:37 -0400

    Integrated git worktrees with telescope.

--- lua/earthtraveller1/plugins/telescope.lua
+++ lua/earthtraveller1/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.2',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim', 'ThePrimeagen/git-worktree.nvim' },
     opts = {},
     config = function(_, opts)
         local builtin = require('telescope.builtin')
@@ -10,6 +10,12 @@ return {
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-        require("telescope").setup(opts)
+        local telescope = require("telescope")
+        telescope.setup(opts)
+        telescope.load_extension("git_worktree")
+
+        vim.keymap.set('n', '<leader>fw', telescope.extensions.git_worktree.git_worktrees, {})
+        vim.keymap.set('n', '<leader>cw', telescope.extensions.git_worktree.create_git_worktree, {})
+
     end
 }
