aab44c8d32433177d59907f9571867a2973b6b82
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-07-29 15:22:35 -0400

    Added a transparent background. Because why not?

--- lua/earthtraveller1/plugins/colorscheme.lua
+++ lua/earthtraveller1/plugins/colorscheme.lua
@@ -1,8 +1,12 @@
 return {
-	'morhetz/gruvbox',
+	'ellisonleao/gruvbox.nvim',
 	lazy = false,
 	priority = 1000,
 	config = function()
+        require("gruvbox").setup({
+            transparent_mode = true,
+        })
+
         vim.cmd.colorscheme("gruvbox")
 	end
 }
