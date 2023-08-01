88d5ccf2ee9ecd3885b8f6460f27bc5368d6bb0b
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-01 13:22:18 -0400

    Added the jump to symbol feature for NeoVim.

--- lua/earthtraveller1/plugins/telescope.lua
+++ lua/earthtraveller1/plugins/telescope.lua
@@ -17,5 +17,7 @@ return {
         vim.keymap.set('n', '<leader>fw', telescope.extensions.git_worktree.git_worktrees, {})
         vim.keymap.set('n', '<leader>cw', telescope.extensions.git_worktree.create_git_worktree, {})
 
+        vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
+
     end
 }
