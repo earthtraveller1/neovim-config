1cd90cb5050d5656f8046448e6fe071b23e46d3f
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-05 20:28:37 -0400

    Setted up ocamllsp and clangd manually.

--- lua/earthtraveller1/plugins/lsp-zero.lua
+++ lua/earthtraveller1/plugins/lsp-zero.lua
@@ -21,6 +21,8 @@ return {
             lsp.default_keymaps({buffer = bufnr})
         end)
 
+        lsp.setup_servers({'clangd', 'ocamllsp'})
+
         -- (Optional) Configure lua language server for neovim
         require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
