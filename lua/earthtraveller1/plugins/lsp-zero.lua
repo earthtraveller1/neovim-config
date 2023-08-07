53a08ff38d8f38b78b28c571778b52ea92655c30
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-07 14:12:10 -0400

    Enabled formatting on save.

--- lua/earthtraveller1/plugins/lsp-zero.lua
+++ lua/earthtraveller1/plugins/lsp-zero.lua
@@ -19,6 +19,7 @@ return {
             -- see :help lsp-zero-keybindings
             -- to learn the available actions
             lsp.default_keymaps({buffer = bufnr})
+            lsp.buffer_autoformat()
         end)
 
         lsp.setup_servers({'clangd', 'ocamllsp'})
