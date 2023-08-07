52e8f4ea0e9d349bc498cbd655d0ad7385b2d54e
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-07 19:54:03 -0400

    Made it so that .vert, .frag, .vs, and .fs files are GLSL files.

--- lua/earthtraveller1.lua
+++ lua/earthtraveller1.lua
@@ -1,3 +1,8 @@
 require("earthtraveller1.settings")
 require("earthtraveller1.keymaps")
 require("earthtraveller1.plugins")
+
+vim.api.nvim_create_autocmd({ "BufNew", "BufNewFile" }, {
+    pattern = {"*.fs", "*.vs", "*.vert", "*.frag"},
+    callback = function() vim.opt_local.filetype = "glsl" end,
+})
