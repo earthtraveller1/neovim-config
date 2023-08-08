67ab61eaad6bf06dcfd8af4eab8054d007be4281
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-07 20:24:50 -0400

    Fixed the GLSL file association mess.

--- lua/earthtraveller1.lua
+++ lua/earthtraveller1.lua
@@ -2,7 +2,9 @@ require("earthtraveller1.settings")
 require("earthtraveller1.keymaps")
 require("earthtraveller1.plugins")
 
-vim.api.nvim_create_autocmd({ "BufNew", "BufNewFile" }, {
-    pattern = {"*.fs", "*.vs", "*.vert", "*.frag"},
-    callback = function() vim.opt_local.filetype = "glsl" end,
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+    pattern = { "*.vert", "*.frag" },
+    callback = function()
+        vim.opt_local.filetype = "glsl"
+    end,
 })
