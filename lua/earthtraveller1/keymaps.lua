10c3d639c8beb8610baeb14b5f7d13906450f0a8
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-15 16:47:13 -0400

    Got rid of the second keymap, since it wasn't cool.

--- lua/earthtraveller1/keymaps.lua
+++ lua/earthtraveller1/keymaps.lua
@@ -10,7 +10,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- Just for me to be cool!
 vim.keymap.set("n", "<leader>rs", "<Cmd>:.!fish<Cr>")
-vim.keymap.set("n", "<leader>rr", "\"pVy@p")
 
 vim.keymap.set("n", "<leader>so", vim.cmd.SymbolsOutline)
 vim.keymap.set("n", "<leader>gi", "<Cmd>Git<Cr><C-w>L50<C-w><")
