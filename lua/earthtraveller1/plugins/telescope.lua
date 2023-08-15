ad1ab5402445bce5fa7f77fabf32dde7d99ec00b
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-08-14 20:13:26 -0400

    Enabled finding hidden files for the find files picker.

--- lua/earthtraveller1/plugins/telescope.lua
+++ lua/earthtraveller1/plugins/telescope.lua
@@ -1,11 +1,12 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.2',
     -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim', 'ThePrimeagen/git-worktree.nvim' },
     opts = {},
     config = function(_, opts)
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ hidden = true }) end, {})
+        vim.keymap.set('n', '<leader>ff', function() builtin.find_files({ hidden = true, no_ignore = false }) end, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
@@ -18,6 +19,5 @@ return {
         vim.keymap.set('n', '<leader>cw', telescope.extensions.git_worktree.create_git_worktree, {})
 
         vim.keymap.set('n', '<leader>fs', builtin.lsp_document_symbols, {})
-
     end
 }
