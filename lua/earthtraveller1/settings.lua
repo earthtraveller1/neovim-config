97194a8edaa439d8d4b0772268a34103cfe543f7
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-01 20:54:46 -0400

    Enabled smart indenting.

--- lua/earthtraveller1/settings.lua
+++ lua/earthtraveller1/settings.lua
@@ -1,10 +1,14 @@
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
+
 vim.opt.guicursor = ""
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
+
 vim.opt.scrolloff = 14
 vim.opt.wrap = false
 vim.opt.cursorline = true
