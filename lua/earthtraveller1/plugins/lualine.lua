a53358b9338904db59dba3b7ea30236a2440e5c8
Yue <liangyuezhao7@gmail.com>
2023-09-07 16:54:02 -0400

    Installed an LSP progress indicator plugin.

--- lua/earthtraveller1/plugins/lualine.lua
+++ lua/earthtraveller1/plugins/lualine.lua
@@ -1,11 +1,27 @@
 return {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "linrongbin16/lsp-progress.nvim",
+    },
+    config = function(_, opts)
+        require("lualine").setup(opts)
+        -- listen lsp-progress event and refresh lualine
+        vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+        vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
+            group = "lualine_augroup",
+            callback = require("lualine").refresh,
+        })
+    end,
     opts = {
         theme = "gruvbox",
         disabled_filetypes = {
             statusline = { "NvimTree" }
+        },
+        sections = {
+            lualine_c = {
+                "require('lsp-progress').progress()"
+            }
         }
     }
 }
-
