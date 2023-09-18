f4ede57992632513784f7ca540655eebedf63c60
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-09-17 22:21:43 -0400

    Configured zls.

--- lua/earthtraveller1/plugins/lsp-zero.lua
+++ lua/earthtraveller1/plugins/lsp-zero.lua
@@ -31,7 +31,7 @@ return {
         end)
 
         lsp.setup_servers({
-            'clangd', 'ocamllsp', 'rust_analyzer', 'pylsp', 'gopls'
+            'clangd', 'ocamllsp', 'rust_analyzer', 'pylsp', 'gopls', 'zls'
         })
 
         -- (Optional) Configure lua language server for neovim
