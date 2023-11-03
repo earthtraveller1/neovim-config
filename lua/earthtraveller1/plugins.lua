3988db3cf84b0c54974743bf19a1b2d334c61e56
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-11-02 20:22:25 -0400

    Installed the Rich Presence plugin.

--- lua/earthtraveller1/plugins.lua
+++ lua/earthtraveller1/plugins.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     require("earthtraveller1.plugins.nvim-surround"),
     require("earthtraveller1.plugins.obsidian"),
     require("earthtraveller1.plugins.oil"),
+    require("earthtraveller1.plugins.presence"),
     require("earthtraveller1.plugins.telescope"),
     require("earthtraveller1.plugins.treesitter"),
     require("earthtraveller1.plugins.undotree"),
