4d1a95b21cc3b38b8143d7fedf46d42152313517
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-11-25 17:48:19 -0500

    Used a darker theme than the default.

--- lua/earthtraveller1/plugins/colorscheme.lua
+++ lua/earthtraveller1/plugins/colorscheme.lua
@@ -20,9 +20,9 @@ return {
         overrides = function(_) -- add/modify highlights
             return {}
         end,
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
+        theme = "dragon", -- Load "wave" theme when 'background' option is not set
         background = { -- map the value of 'background' option to a theme
-            dark = "wave", -- try "dragon" !
+            dark = "dragon", -- try "dragon" !
             light = "lotus"
         },
     },
