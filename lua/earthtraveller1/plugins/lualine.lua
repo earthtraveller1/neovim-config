ad335f26c110d9b1e2fcb7fcf6b559201c127519
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2023-11-25 20:35:36 -0500

    Used the right theme, along with removing uneeded stuff.

--- lua/earthtraveller1/plugins/lualine.lua
+++ lua/earthtraveller1/plugins/lualine.lua
@@ -4,17 +4,8 @@ return {
         "nvim-tree/nvim-web-devicons",
         "linrongbin16/lsp-progress.nvim",
     },
-    config = function(_, opts)
-        require("lualine").setup(opts)
-        -- listen lsp-progress event and refresh lualine
-        vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-        vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
-            group = "lualine_augroup",
-            callback = require("lualine").refresh,
-        })
-    end,
     opts = {
-        theme = "gruvbox",
+        theme = "kanagawa",
         disabled_filetypes = {
             statusline = { "NvimTree" }
         },
