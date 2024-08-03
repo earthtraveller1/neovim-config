b67411b4141deefcab6bb6b911af10d8a6b0fcb1
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2024-08-03 14:29:56 -0400

    Changed the colorscheme to catppuccin.

--- lua/earthtraveller1/plugins/colorscheme.lua
+++ lua/earthtraveller1/plugins/colorscheme.lua
@@ -1,34 +1,17 @@
 return {
-    'rebelot/kanagawa.nvim',
-    lazy = false,
+    "catppuccin/nvim",
+    name = "catppuccin",
     priority = 1000,
     opts = {
-        compile = false, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = false, -- do not set background color
-        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        colors = {         -- add/modify theme and palette colors
-            palette = {},
-            theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-        },
-        overrides = function(_) -- add/modify highlights
-            return {}
-        end,
-        theme = "dragon", -- Load "wave" theme when 'background' option is not set
-        background = { -- map the value of 'background' option to a theme
-            dark = "dragon", -- try "dragon" !
-            light = "lotus"
-        },
+        dim_inactive = {
+            enabled = true,
+            shade = "dark",
+            percentage = 0.15,
+        }
     },
     config = function(_, opts)
-        require("kanagawa").setup(opts)
+        require("catppuccin").setup(opts)
 
-        vim.cmd.colorscheme("kanagawa")
+        vim.cmd.colorscheme("catppuccin-mocha")
     end
 }
