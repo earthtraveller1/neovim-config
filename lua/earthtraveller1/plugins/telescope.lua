040792b215d79432c735d3199344efbc86929351
Yue <yue@localhost.localdomain>
2025-12-24 23:19:26 -0500

    Upgraded to the newest version of telescope.

--- lua/earthtraveller1/plugins/telescope.lua
+++ lua/earthtraveller1/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    tag = 'v0.2.0',
     -- or                              , branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim', 'ThePrimeagen/git-worktree.nvim' },
     opts = {},
